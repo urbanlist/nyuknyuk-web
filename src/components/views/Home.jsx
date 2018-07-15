4669e7f238062a6cd8f42b800041f9a3c98be980
huinalam <huinalam@gmail.com>
2018-07-15 15:33:09 +0900

    WeatherController를 통해 날씨를 읽어와 표현할 수 있도록 함

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,23 +1,78 @@
 import React from 'react';
 import cloud from '../../../assets/weather/cloud.svg';
 import Random from '../modules/Random.js';
+import WeatherController from '../modules/WeatherController.js';
+import Timer from '../modules/Timer.js';
 import './Home.styl';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       cloudPositions: [
         Random(0,window.innerWidth),
         Random(0,window.innerWidth),
         Random(0,window.innerWidth),
-        Random(0,window.innerWidth)]
+        Random(0,window.innerWidth)],
+      date:'-',
+      temperature: '-',
+      skyStatus: '-',
+      windSpeed: '-',
+      color: {
+        start: {
+          red: 0,
+          green: 0,
+          blue: 0
+        },
+        end: {
+          red: 0,
+          green: 0,
+          blue: 0
+        },
+      }
     };
+
+    this.weatherController = new WeatherController();
+    this.weatherTimer = new Timer(this.setWeather.bind(this), 60 * 1000);
+    this.clockTimer = new Timer(this.setClock.bind(this), 60 * 1000);
   }
 
   componentDidMount() {
+    this.weatherTimer.start();
+    this.clockTimer.start();
     this.timer = window.setTimeout(this.moveCloud(), 100);
     this.timer = window.setInterval(this.moveCloud(), 4000);
+  }
+
+  componentWillUnmount() {
+    window.clearTimeout(this.timer);
+    this.weatherTimer.stop();
+    this.clockTimer.stop();
+  }
+
+  setWeather() {
+    this.weatherController.get(data => {
+      this.setState({
+        temperature: data.temperature,
+        skyStatus: data.sky.status,
+        windSpeed: data.wind.speed,
+        color: data.color
+      });
+    });
+  }
+
+  setClock() {
+    let date = new Date();
+    let month = date.getMonth()+1;
+    let day = date.getDate();
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+
+    this.setState({
+      date: hour + ':' + min + ' ' + day + ', ' + month + ', ' + year
+    });
   }
 
   moveCloud() {
@@ -35,10 +90,6 @@ class Home extends React.Component {
         cloudPositions: arr
       });
     };
-  }
-
-  componentWillUnmount() {
-    window.clearTimeout(this.timer);
   }
 
   render() {
@@ -59,12 +110,17 @@ class Home extends React.Component {
         </div>
       )
     }
+
+    let color = this.state.color;
+    let background = `background linear-gradient(to bottom,rgb(${color.start.red}, ${color.start.green}, ${color.start.blue}) , rgb(${color.end.red}, ${color.end.green}, ${color.end.blue}))`;
     
     return (
-      <div className="home">
+      <div className="home" style={{
+       "background" : background
+      }}>
         <div className="top">
           <div className="weather">
-            {"Sunny, 20°C"}
+            {this.state.skyStatus + ", " + this.state.temperature + "°C"}
           </div>
           <div className="place">
             {"SEOUL"}
@@ -73,7 +129,7 @@ class Home extends React.Component {
         <div className="center">
           <div>
             <div className="datetime">
-              {"16:27, 01, July, 2018"}
+              {this.state.date}
             </div>
             <div className="content">
               {"북미, 일부 성과에도 입장차 확인…후속협상에 공 넘겨"}
