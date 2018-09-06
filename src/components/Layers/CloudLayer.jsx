3baba6db90ee6b8b511685b1ac3983229d70602b
huinalam <huinalam@gmail.com>
2018-09-06 22:27:34 +0900

    구름 종류별로 가능하도록 함

--- src/components/Layers/CloudLayer.jsx
+++ src/components/Layers/CloudLayer.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ConvertWindSpeedToPixel from '../modules/WindSpeed.js';
-import cloud1 from '../../../assets/weather/cloud.svg';
-import cloud2 from '../../../assets/weather/cloud2.svg';
-import cloud3 from '../../../assets/weather/cloud3.svg';
+import classnames from 'classnames';
 import Random from '../modules/Random.js';
 import './CloudLayer.styl';
 
@@ -21,25 +19,31 @@ class CloudLayer extends React.Component {
     let cloudType = this.props.cloudType;
     let seconds = ConvertWindSpeedToPixel(this.props.windSpeed);
 
+    let cloudClass = classnames({
+      "cloud": true,
+      "cloud-type-1": cloudType == 1,
+      "cloud-type-2": cloudType == 2,
+    });
+
     // todo: size에 따라 속도를 다르게 주어야함
     return (
       <div className="cloud-layer">
-        <div className="cloud cloud-more" style={{
+        <div className={cloudClass} style={{
           "animation": `cloud ${seconds + Random(-3,7)}s ${Random(0, seconds)/3}s infinite linear`
         }}></div>
-        <div className="cloud cloud-more" style={{
+        <div className={cloudClass} style={{
           "animation": `cloud ${seconds + Random(-3,7)}s ${Random(0, seconds)/3}s infinite linear`
         }}></div>
-        <div className="cloud cloud-more" style={{
+        <div className={cloudClass} style={{
           "animation": `cloud ${seconds + Random(-3,7)}s ${Random(0, seconds)/3}s infinite linear`
         }}></div>
-        <div className="cloud cloud-more" style={{
+        <div className={cloudClass} style={{
           "animation": `cloud ${seconds + Random(-3,7)}s ${Random(0, seconds)/3}s infinite linear`
         }}></div>
-        <div className="cloud cloud-more" style={{
+        <div className={cloudClass} style={{
           "animation": `cloud ${seconds + Random(-3,7)}s ${Random(0, seconds)/3}s infinite linear`
         }}></div>
-        <div className="cloud cloud-more" style={{
+        <div className={cloudClass} style={{
           "animation": `cloud ${seconds + Random(-3,7)}s ${Random(0, seconds)/3}s infinite linear`
         }}></div>
       </div>
