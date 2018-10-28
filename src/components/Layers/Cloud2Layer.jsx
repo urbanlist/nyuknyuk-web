fcce048a951623774fc48bf9f33de60ce1b43d81
huinalam <huinalam@gmail.com>
2018-10-28 19:31:47 +0900

    흐림 & 구름 많음일 때 구름 크기를 화면 크게 맞춤

--- src/components/Layers/Cloud2Layer.jsx
+++ src/components/Layers/Cloud2Layer.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import classNames from 'classnames';
 import './Cloud2Layer.styl';
+
+
+const createItemSize = (windowSize) => {
+  let itemSize = 1;
+  if (windowSize < 400) {
+    itemSize = 1;
+  } else if (windowSize < 600) {
+    itemSize = 2;
+  } else {
+    itemSize = 3;
+  }
+  return itemSize;
+}
+
+
+const createAnimationName = (size) => {
+  if (size == 1) {
+    return "cloud2layer-ani";
+  } else if (size == 2) {
+    return "cloud2layer-ani-size2";
+  } else if (size == 3) {
+    return "cloud2layer-ani-size3";
+  }
+  return "cloud2layer-ani";
+}
 
 
 class Cloud2Layer extends React.Component {
@@ -9,13 +35,19 @@ class Cloud2Layer extends React.Component {
 
   render() {
     let seconds = 15;
+    let windowHalfHeight = Math.floor(
+      window.innerHeight/2
+    );
+    let itemSize = createItemSize(windowHalfHeight);
+    let animationName = createAnimationName(itemSize);
 
-    let overcastCount = Math.floor(
-      window.innerHeight/360
-    ) + 1;
-    let overcasts = new Array(overcastCount).fill(1).map((_, i) => {
-      return (<div key={i} className="cloud2layer-item" style={{
-        "animation": `overcast ${seconds}s linear infinite`
+    let overcasts = new Array(2).fill(1).map((_, i) => {
+      return (<div key={i} className={classNames({
+        "cloud2layer-item": itemSize == 1,
+        "cloud2layer-item-size2": itemSize == 2,
+        "cloud2layer-item-size3": itemSize == 3
+      })} style={{
+        "animation": `${animationName} ${seconds}s linear infinite`
       }}></div>);
     });
 
--- src/components/Layers/OvercastLayer.jsx
+++ src/components/Layers/OvercastLayer.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import classNames from 'classnames';
 import './OvercastLayer.styl';
+
+
+const createItemSize = (windowSize) => {
+  let itemSize = 1;
+  if (windowSize < 400) {
+    itemSize = 1;
+  } else if (windowSize < 600) {
+    itemSize = 2;
+  } else {
+    itemSize = 3;
+  }
+  return itemSize;
+}
+
+
+const createAnimationName = (size) => {
+  if (size == 1) {
+    return "overcast";
+  } else if (size == 2) {
+    return "overcast-size2";
+  } else if (size == 3) {
+    return "overcast-size3";
+  }
+  return "overcast";
+}
 
 
 class OvercastLayer extends React.Component {
@@ -12,15 +38,31 @@ class OvercastLayer extends React.Component {
 
   render() {
     let seconds = 15;
-
     let overcastCount = Math.floor(
       window.innerHeight/180
     ) + 1;
+
+    let windowHalfHeight = Math.floor(
+      window.innerHeight/2
+    );
+    let itemSize = createItemSize(windowHalfHeight);
+    let animationName = createAnimationName(itemSize);
+
     let overcasts = new Array(overcastCount).fill(1).map((_, i) => {
-      return (<div key={i} className="overcast-item" style={{
-        "animation": `overcast ${seconds}s linear infinite`
+      return (<div key={i} className={classNames({
+        "overcast-item": itemSize == 1,
+        "overcast-item-size2": itemSize == 2,
+        "overcast-item-size3": itemSize == 3
+      })} style={{
+        "animation": `${animationName} ${seconds}s linear infinite`
       }}></div>);
     });
+
+    // let overcasts = new Array(overcastCount).fill(1).map((_, i) => {
+    //   return (<div key={i} className="overcast-item" style={{
+    //     "animation": `overcast ${seconds}s linear infinite`
+    //   }}></div>);
+    // });
 
     return (
       <div className="overcast-layer">
