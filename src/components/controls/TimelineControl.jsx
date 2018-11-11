8fee7503dcaeda2b598fdb3c11bbfb2b34537eba
huinalam <huinalam@gmail.com>
2018-11-11 16:50:22 +0900

    터치 스크롤에 따른 이동 기능 추가

--- src/components/controls/TimelineControl.jsx
+++ src/components/controls/TimelineControl.jsx
@@ -46,15 +46,73 @@ class TimelineControl extends React.Component {
     });
   }
 
+  _onPointClick1() {
+    if (this.props.onPointClick)
+      this.props.onPointClick({
+        color: {
+          start: {
+            red: 30,
+            green: 30,
+            blue: 30
+          },
+          end: {
+            red: 40,
+            green: 40,
+            blue: 40
+          }
+        },
+        skyStatus: "SKY_A03"
+      });
+  }
+
+  _onPointClick2() {
+    if (this.props.onPointClick)
+      this.props.onPointClick({
+        color: {
+          start: {
+            red: 20,
+            green: 10,
+            blue: 10
+          },
+          end: {
+            red: 40,
+            green: 30,
+            blue: 30
+          }
+        },
+        skyStatus: "SKY_A11"
+      });
+  }
+
+  _onTimelineTouchStart(e) {
+    this.timelineTouchStartClientX = e.touches[0].clientX;
+    this.isTimelineTouchStart = true;
+  }
+
+  _onTimelineTouchMove(e) {
+    if (!this.isTimelineTouchStart)
+      return;
+
+    let touchClientX = e.touches[0].clientX;
+    if (this.timelineTouchStartClientX - touchClientX > 50) {
+      this._moveToRight();
+      this.isTimelineTouchStart = false;
+    } else if (this.timelineTouchStartClientX - touchClientX < -50) {
+      this._moveToLeft();
+      this.isTimelineTouchStart = false;
+    }
+  }
+
   render() {
     // transform translate3d(200px, 4px, 0)
 
     return (
       <div className="timeline-control">
         <div className="margin"></div>
-        <button className="left" onClick={e => this._moveToLeft()}>(-</button>
+        <button className="left" onClick={e => this._moveToLeft()} >(-</button>
         <div className="margin"></div>
-        <div className="timeline-bar-control">
+        <div className="timeline-bar-control"
+          onTouchStart={e => this._onTimelineTouchStart(e)} onTouchMove={e => this._onTimelineTouchMove(e)}>
           <div className="timeline-bar" style={this.state.timelineBarStyle} ref={this.timelineBar}>
             <div className="start-text">
               <span>2018.01.01</span>
@@ -62,12 +120,12 @@ class TimelineControl extends React.Component {
             <div className="start-bar"></div>
             <div className="middle-bar">
               <div className="bar"></div>
-              <button className="time-point-button" style={{
+              <button className="time-point-button" onClick={e => this._onPointClick1()} style={{
                 left: 50
               }}>
                 <div className="time-point"></div>
               </button>
-              <button className="time-point-button" style={{
+              <button className="time-point-button" onClick={e => this._onPointClick2()} style={{
                 left: 150
               }}>
                 <div className="time-point"></div>
